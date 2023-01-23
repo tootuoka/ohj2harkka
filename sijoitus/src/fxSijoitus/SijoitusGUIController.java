fd4f3b8f086f857f6ebd1e0221f8882ede8c5e51
tootuoka <tomi.o.u.kankaanpaa@student.jyu.fi>
2023-01-23 12:49:45 +0200

    uusi pohja luotu

---
+++ sijoitus/src/fxSijoitus/SijoitusGUIController.java
@@ -0,0 +1,10 @@
+package fxSijoitus;
+
+/**
+ * @author tomikankaanpaa
+ * @version 20.1.2023
+ *
+ */
+public class SijoitusGUIController {
+      // TODO
+}
---
+++ sijoitus/src/fxSijoitus/SijoitusMain.java
@@ -0,0 +1,38 @@
+package fxSijoitus;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author tomikankaanpaa
+ * @version 20.1.2023
+ *
+ */
+public class SijoitusMain extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("SijoitusGUIView.fxml"));
+            final Pane root = ldr.load();
+            //final SijoitusGUIController sijoitusCtrl = (SijoitusGUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("sijoitus.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Sijoitus");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei käytössä
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
